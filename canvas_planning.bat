aba519d277de0f63c58ca22f1f743f133f88b146
Paul Gower <pgower@stmonicas.qld.edu.au>
2016-12-06 15:48:23 +1000

    Experimenting with converting some of my old YAML into PETL data structures and how that works. (account_lookup_table.csv, time_periods.csv) Creating 2 new lookup plugins to filter or fill values based on the contents of another data table.

--- canvas_planning.bat
+++ canvas_planning.bat
@@ -1,22 +1,24 @@
 ECHO off
 python main.py ptf9_course_import -i source\timetable_planning.ptf9 -d ptf9_courses ^
+               regex_filter -s ptf9_courses -m exclude -f course_id "^[BGLR]{1}[1-9]{1}$" ^
+                                                  -f Code "^GP-[0-9]{2}ACT.*$" ^
+                                                  -f Code "^ASM-.*$" ^
+                                                  -f Code "^.+QCS.+$" ^
+                                                  -f Code "^PAS-.*$" ^
+                                                  -f Code "^BREAK-.*$" ^
+                                                  -f Code "^[0-9]{2}ASM.*$" ^
+                                                  -f Code "^ACT$" ^
+                                                  -f Code "^[0-9]{2}ACT$" ^
+                                                  -f Code "^.+WRK.*$" ^
+                                                  -f Code "^[0-9]{2}WRK.*$" ^
+                                                  -f Code "^[0-9]{2}(STU|LAN).*$" ^
                copy_columns -s ptf9_courses -d temp_courses -c Code -c Name ^
                rename_columns -s temp_courses -r Code course_id -r Name long_name ^
                copy_columns -s temp_courses -d courses -c course_id -c long_name ^
                fill_column -s courses -c short_name -v "course_id" ^
                fill_column -s courses -c status -v "'active'" ^
-               regex_filter -s courses -m exclude -f course_id "^[BGLR]{1}[1-9]{1}$" ^
-                                                  -f course_id "^GP-[0-9]{2}ACT.*$" ^
-                                                  -f course_id "^ASM-.*$" ^
-                                                  -f course_id "^.+QCS.+$" ^
-                                                  -f course_id "^PAS-.*$" ^
-                                                  -f course_id "^BREAK-.*$" ^
-                                                  -f course_id "^[0-9]{2}ASM.*$" ^
-                                                  -f course_id "^ACT$" ^
-                                                  -f course_id "^[0-9]{2}ACT$" ^
-                                                  -f course_id "^.+WRK.*$" ^
-                                                  -f course_id "^[0-9]{2}WRK.*$" ^
-                                                  -f course_id "^[0-9]{2}(STU|LAN).*$" ^
-               print -s courses -n 5
+
+               csv_import -i account_lookup_table.csv -d account_lookup_table ^
+               print -s account_lookup_table -n 5
 
 
