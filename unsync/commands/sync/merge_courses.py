4559a8a3bd78e271f90df1ca5a3a9e723bf4632f
Paul Gower <pgower@gmail.com>
2017-03-16 17:08:24 +1000

    Modified some unicode types to str for the py2 to py3 migration. Added an extra qualifier to the save_removed_courses conditional to only run if there is actually some merge_data to work with. When no merge_data was provided this was causing an error.

--- unsync/commands/sync/merge_courses.py
+++ unsync/commands/sync/merge_courses.py
@@ -1,4 +1,5 @@
 """Canvas specific command to merge specified courses together."""
+from __future__ import unicode_literals
 
 import click
 from unsync.lib.unsync_data import pass_data
@@ -6,10 +7,10 @@ from unsync.lib.unsync_commands import unsync
 
 
 @unsync.command()
-@click.option('--courses', required=True, type=unicode, help='The source courses table containing courses to be merged.')
-@click.option('--course-id-field', required=True, type=unicode, default='course_id', help='The field name for the courses id. Defaults to Canvas standard course_id.')
-@click.option('--course-name-field', required=True, type=unicode, default='long_name', help='The field name for the course name. Defaults to Canvas standard long_name.')
-@click.option('--merge-data', required=True, type=unicode, help='Table containing pairs of Merged Name -> Source Name mappings.')
+@click.option('--courses', required=True, type=str, help='The source courses table containing courses to be merged.')
+@click.option('--course-id-field', required=True, type=str, default='course_id', help='The field name for the courses id. Defaults to Canvas standard course_id.')
+@click.option('--course-name-field', required=True, type=str, default='long_name', help='The field name for the course name. Defaults to Canvas standard long_name.')
+@click.option('--merge-data', required=True, type=str, help='Table containing pairs of Merged Name -> Source Name mappings.')
 @click.option('--save-removed-courses/--no-save-removed-courses', default=True, help='If True then save the removed course data in a table specificed by removed-courses-destination.')
 @click.option('--removed-courses-destination', default='removed_merged_course_data', help='The destination table for any courses that were removed as part of the merge.')
 @pass_data
@@ -27,7 +28,7 @@ def merge_courses(data, courses, course_id_field, course_name_field, merge_data,
     courses_table = courses_table.select(course_id_field, lambda v: v not in source_course_ids_to_remove)
 
     # Save the removed courses to a seperate table for later use
-    if save_removed_courses:
+    if save_removed_courses and merge_data.nrows() > 0:
         source_courses_to_remove = (source_courses_to_remove
                                     .lookupjoin(merge_data, lkey='course_id', rkey='source_id')
                                     # .cut('course_id', 'merged_id')
