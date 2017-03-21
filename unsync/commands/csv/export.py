ed91f200b5c1884c585e0f933114a68f50b8e6cf
Paul Gower <pgower@gmail.com>
2017-03-21 13:54:00 +1000

    Updated all of the csv commands to use str types instead of unicode for python3 compatibility.

--- unsync/commands/csv/export.py
+++ unsync/commands/csv/export.py
@@ -1,4 +1,6 @@
 """CSV Import and Export functions for CanvasUnsync."""
+from __future__ import unicode_literals
+
 import click
 import petl
 
@@ -9,7 +11,7 @@ from unsync.lib.unsync_commands import unsync
 @unsync.command()
 @click.option('--output-file', '-o', type=click.Path(dir_okay=False, readable=True, resolve_path=True), help='CSV file that data will be read from.')
 @click.option('--source', '-s', required=True, help='Source table to export as CSV.')
-@click.option('--csv-arg', multiple=True, type=click.Tuple([unicode, unicode]), help='Arguments that will be passed to petl\'s CSV parsing engine.')
+@click.option('--csv-arg', multiple=True, type=click.Tuple([str, str]), help='Arguments that will be passed to petl\'s CSV parsing engine.')
 @click.option('--errors', default='strict', help='PETL option for CSV errors.')
 @click.option('--write-header/--no-write-header', default=True, help='When set the CSV file will have a header row.')
 @click.option('--append/--no-append', default=False, help='When set the output file will be opened and rows will be appended to the existing data. When set --write-header is always False.')
--- unsync/commands/csv/full_export.py
+++ unsync/commands/csv/full_export.py
@@ -1,4 +1,6 @@
 """CSV Import and Export functions for CanvasUnsync."""
+from __future__ import unicode_literals
+
 import click
 import petl
 import os
@@ -9,7 +11,7 @@ from unsync.lib.unsync_commands import unsync
 
 @unsync.command()
 @click.option('--output-dir', '-o', type=click.Path(dir_okay=True, file_okay=False, readable=True, resolve_path=True), help='CSV file that data will be read from.')
-@click.option('--csv-arg', multiple=True, type=click.Tuple([unicode, unicode]), help='Arguments that will be passed to petl\'s CSV parsing engine.')
+@click.option('--csv-arg', multiple=True, type=click.Tuple([str, str]), help='Arguments that will be passed to petl\'s CSV parsing engine.')
 @click.option('--errors', default='strict', help='PETL option for CSV errors.')
 @click.option('--write-header/--no-write-header', default=True, help='When set the CSV file will have a header row.')
 @click.option('--exclude-empty/--include-empty', default=True, help='When set data tables with no data in them will not create CSV files.')
