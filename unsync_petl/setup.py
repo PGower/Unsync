0f61f1c0bb0e3a7d2c85f9e689e47e77149991b3
Paul Gower <pgower@gmail.com>
2017-06-21 14:14:15 +1000

    Trimmed unsync_petl/setup.py

--- unsync_petl/setup.py
+++ unsync_petl/setup.py
@@ -10,17 +10,8 @@ setup(
     version=unsync_petl.__version__,
     package_dir={'unsync_petl': 'unsync_petl'},
     packages=find_packages(),
-    # package_data={
-    #     'templates': ['unsync/templates/*.*', 'unsync/templates/*'],
-    # },
-    # include_package_data=True,
     install_requires=[
         'unsync',
         'petl',
     ],
-    # entry_points={
-    #     'console_scripts': [
-    #         'unsync=unsync.command:cli',
-    #     ]
-    # }
 )
