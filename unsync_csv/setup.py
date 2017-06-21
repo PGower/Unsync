96e1a4c9f50abda44ad7ea125bbb013ec77dcd56
Paul Gower <pgower@gmail.com>
2017-06-21 14:03:39 +1000

    Trimmed unsync_csv/setup.py

--- unsync_csv/setup.py
+++ unsync_csv/setup.py
@@ -3,11 +3,6 @@ from __future__ import absolute_import
 from setuptools import setup, find_packages
 import unsync_csv
 
-# BASE_PATH = os.path.dirname(os.path.abspath(__file__))
-
-# PACKAGES = find_packages(os.path.join(BASE_PATH, 'unsync'))
-
-# print PACKAGES
 
 setup(
     name='unsync_csv',
@@ -15,17 +10,8 @@ setup(
     version=unsync_csv.__version__,
     package_dir={'unsync_csv': 'unsync_csv'},
     packages=find_packages(),
-    # package_data={
-    #     'templates': ['unsync/templates/*.*', 'unsync/templates/*'],
-    # },
-    # include_package_data=True,
     install_requires=[
         'unsync',
         'petl',
     ],
-    # entry_points={
-    #     'console_scripts': [
-    #         'unsync=unsync.command:cli',
-    #     ]
-    # }
 )
